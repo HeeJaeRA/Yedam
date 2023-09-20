31b52e5237569ff81975862850372c8fa07948d7
HeeJae RA <rhj7369@nate.com>
2023-09-20 20:35:22 +0900

    .

--- DB_Oracle/그룹함수 예제.sql
+++ DB_Oracle/그룹함수 예제.sql
@@ -113,10 +113,6 @@ FROM jobs j LEFT JOIN employees e ON(j.job_id = e.job_id)
 ORDER BY j.job_id, e.first_name;
 
 -- 1. Zlotkey와 동일한 부서에 속한 모든 사원의 이름과 입사일을 표시하는 질의를 작성하시오. Zlotkey는 결과에서 제외하시오.
-SELECT department_id
-FROM employees
-WHERE INITCAP(last_name) = 'Zlotkey';
-
 SELECT last_name, hire_date
 FROM employees
 WHERE department_id = 
@@ -126,15 +122,15 @@ WHERE department_id =
 AND INITCAP(last_name) != 'Zlotkey';
 
 -- 2. 급여가 평균 급여보다 많은 모든 사원의 사원 번호와 이름을 표시하는 질의를 작성하고 결과를 급여에 대해 오름차순으로 정렬하시오.
-SELECT employee_id, first_name
+SELECT employee_id, first_name, salary
 FROM employees
-WHERE salary > ANY
+WHERE salary > ALL
     (SELECT AVG(salary)
      FROM employees)
 ORDER BY salary;
 
 -- 3. 이름에 u가 포함된 사원과 같은 부서에서 일하는 모든 사원의 사원 번호와 이름을 표시하는 질의를 작성하고 질의를 실행하시오.
-SELECT employee_id, first_name
+SELECT employee_id, last_name
 FROM employees
 WHERE department_id = ANY
     (SELECT department_id
@@ -156,5 +152,23 @@ WHERE manager_id = ANY
     (SELECT employee_id
      FROM employees
      WHERE last_name = 'King');
+
 -- 6. Executive 부서의 모든 사원에 대한 부서 번호, 이름, 업무 ID를 표시하시오.
+SELECT department_id, last_name, job_id
+FROM employees
+WHERE department_id = ANY
+    (SELECT department_id
+     FROM departments
+     WHERE department_name = 'Executive');
+     
 -- 7. 평균 급여보다 많은 급여를 받고, 이름에 u가 포함된 사원과 같은 부서에 근무하는 모든 사원의 사원번호, 이름, 급여를 표시하시오.
+SELECT employee_id, last_name, salary
+FROM employees
+WHERE salary > ALL
+    (SELECT AVG(salary)
+     FROM employees)
+AND department_id = ANY
+    (SELECT department_id
+     FROM employees
+     WHERE UPPER(last_name) LIKE '%U%')
+ORDER BY salary;
